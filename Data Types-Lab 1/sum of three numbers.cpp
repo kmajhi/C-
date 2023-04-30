8d78a7b3a2b4529dd35a6514e5ccb1643667a948
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-04-30 23:26:34 +0600

    Create sum of three numbers.cpp

---
+++ Data Types-Lab 1/sum of three numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, num3, sum;
+
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    sum = num1 + num2 + num3;
+
+    cout << "The sum of the three numbers is: " << sum << endl;
+
+    return 0;
+}
